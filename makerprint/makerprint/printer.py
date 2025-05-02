67e319c22c5922f5c5a8fbda660ca9de8f1ed629
Maxime Ellerbach <maxime@ellerbach.net>
2025-05-02 12:01:56 +0200

    cleanup

--- makerprint/makerprint/printer.py
+++ makerprint/makerprint/printer.py
@@ -67,10 +67,7 @@ class Printer(printcore):
     def get_status(self):
         progress = 0
         if self.mainqueue:
-            # (layer, _) = self.mainqueue.idxs(self.queueindex)
-            # (last_layer, _) = self.mainqueue.idxs(-1)
-            # progress = int(layer * 100 / last_layer)
-            progress = int(float(self.queueindex) / len(self.p.mainqueue) * 100)
+            progress = int(float(self.queueindex) / len(self.mainqueue) * 100)
 
         return models.PrinterStatus(
             connected=self.online,
